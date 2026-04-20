6464d091a0174bf213b0ab14e18030290fadf570
rb2268 <rbj2268@gmail.com>
2026-04-19 21:06:56 -0400

    Co-authored-by: Joon Heo <JoonChicken@users.noreply.github.com> Co-authored-by: LLai0_0 <LLai1@users.noreply.github.com>

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stm32l432xx.h>
+#include "ee14lib.h"
+
+
+#define I2C_SCL_PIN D1
+#define I2C_SDA_PIN D0
+#define BARO_I2C_ADDR 0x76
+
+
+// This function is called by printf() to handle the text string
+// We want it to be sent over the serial terminal, so we just delegate to that function
+int _write(int file, char *data, int len) {
+    serial_write(USART2, data, len);
+    return len;
+}
+
+void delay_ms(uint32_t delay) {
+    uint32_t ms = systick_counter;
+    ms += delay;
+    while(systick_counter < ms) {
+    }
+    
+}
+
+
+int main()
+{
+    host_serial_init(9600);
+    i2c_init(I2C1, I2C_SCL_PIN, I2C_SDA_PIN);
+
+    while (true) {
+
+        
+        i2c_read(I2C1, BARO_I2C_ADDR data, 1);// Figure out how to actually read, idk
+        
+    }
+}
