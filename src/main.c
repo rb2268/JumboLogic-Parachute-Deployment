ac99abbd4d74e27524cddc2622957373a7b972cc
LLai1 <ljcooll236@gmail.com>
2026-04-30 04:28:40 -0400

    fixed main

--- src/main.c
+++ src/main.c
@@ -32,16 +32,71 @@ void config_gpio_interrupt(void)
 }
 
 int main()
-
 {
-
     host_serial_init(9600);
     i2c_init(I2C1, I2C_SCL_PIN, I2C_SDA_PIN);
-
     SysTick_Init();
-    setupButtons(A4);
+    bmp388_init();
+    config_gpio_interrupt();
+
+    for (volatile int i = 0; i < 20; i++) {
+        printf("I'm alive!\n");
+    }
+
+    State_Data_t flight_state = init_staging();
+
+    uint16_t ref_collect_cnt = 0;
+    bool ref_altitude_set = false;
+    float ref_pressure = 0;
+    float ref_altitude = 0;
+
+    bool motor_opened = false;
+
+    motor_init(TIM16);
 
     while (true) {
-        runButtons(A4);
+        printf("read: %f", bmp388_process());
+        /*
+        LOGIC:
+        COLLECT BARO SAMPLE
+        COLLECT IMU SAMPLE
+
+        IF (STANDBY and ref_collect_cnt < 250):
+            ref_pressure = (ref_pressure * ref_collect_cnt + BARO SAMPLE) / (++ref_collect_cnt)
+        ELSE IF (NOT ref_altitude_set):
+            ref_altitude_set = true
+            ref_altitude = pressure_to_altitude_f(ref_pressure)
+        
+        float imu_mag = 0;
+        if (ACCEL_LOCKOUT):
+            imu_mag = sqrt(IMU_SAMPLE_X ^ 2 + IMU_SAMPLE_Y ^ 2 + IMU_SAMPLE_Z ^ 2)
+
+        flight_state = next_state(flight_state, BARO SAMPLE, ref_pressure, imu_mag, get_time());
+
+        IF (flight_state.state > STANDBY):
+            
+        */
+
+        
+        if (get_time() % 2000 < 1000) {
+            if (!motor_opened){
+                NVIC_DisableIRQ(SysTick_IRQn);
+                motor_open(TIM16, A5);
+                NVIC_EnableIRQ(SysTick_IRQn);
+                motor_opened = true;
+            }
+        } else  {
+            if (motor_opened) {
+                NVIC_DisableIRQ(SysTick_IRQn);
+                motor_close(TIM16, A5);
+                NVIC_EnableIRQ(SysTick_IRQn);
+                motor_opened = false;
+            }
+        }
+
+        setupButtons(A4);
+
+        while (true) {
+            runButtons(A4);
+        }
     }
-}
